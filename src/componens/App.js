cb967921eed59eb62696c35fa4fce7e433e97fc7
Arkadiusz Szumacher <arek.szumacher@gmail.com>
2018-11-20 14:34:51 +0100

    Initial commit

---
+++ src/componens/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+
+    render(){
+        return <div><i>Witamy!</i></div>
+    }
+
+}
+
+export default App; 
---
+++ src/componens/Table.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Tabel extends React.Component {
+    render (){
+        return <ul>
+            <li>
+                Nowy item
+            </li>
+            <li>
+                Nowy item
+            </li>
+            <button class="btn btn-success">Przycisk</button>
+        </ul>
+    }
+}
+
+export default Tabel;
