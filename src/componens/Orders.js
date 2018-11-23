a70ab980a9b8fc2d639ea377433a773aa9adb02d
Arkadiusz Szumacher <arek.szumacher@gmail.com>
2018-11-23 11:08:39 +0100

    Add key

--- src/componens/Orders.js
+++ src/componens/Orders.js
@@ -7,7 +7,7 @@ class Orders extends React.Component {
   render() {
 
     const orderedBooks = this.props.order.map(order => {
-      return <OrderView key={book.name} book={order} removeFromOrder={this.props.removeFromOrder}/>
+      return <OrderView key={order.name} book={order} removeFromOrder={this.props.removeFromOrder}/>
     })
 
     return (
